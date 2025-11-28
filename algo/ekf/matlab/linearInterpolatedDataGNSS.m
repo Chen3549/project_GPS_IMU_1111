bb00274ce6f6422c53b6d46d4110d1c640c58c39
chenqiang <jstzjychen@163.com>
2025-11-28 10:52:47 +0800

    GNSS data drawn by google map & split draw data utils

--- algo/ekf/matlab/linearInterpolatedDataGNSS.m
+++ algo/ekf/matlab/linearInterpolatedDataGNSS.m
@@ -1,10 +1,9 @@
 function [dataGNSSInterpolated, dataIMUInterpolated] = linearInterpolatedDataGNSS(dataGNSS, dataIMU)
 % linear interpolated GNSS data
-% input: data GNSS
-%        data IMU
-% output:
-% data GNSS interpolated
-% data IMU interpolated
+% input:  data GNSS
+%         data IMU
+% output: data GNSS interpolated
+%         data IMU interpolated
 %
 dataGNSSInterpolated = zeros(size(dataIMU, 1), size(dataGNSS, 2));
 dataIMUInterpolated = zeros(size(dataIMU));
@@ -12,31 +11,6 @@ dataIMUInterpolated = zeros(size(dataIMU));
 interp_cnt = 0;
 j = 1; % GNSS data iterate
 for i = 1:height(dataIMU) % IMU data iterate
-    % if j > height(dataGNSS)
-    %     break;
-    % end
-    % if dataGNSS(j, 1) == dataIMU(i, 1)
-    %     interp_cnt = interp_cnt + 1;
-    %     dataGNSSInterpolated(interp_cnt, :) = dataGNSS(j, :);
-    %     dataIMUInterpolated(interp_cnt, :) = dataIMU(i, :);
-    % else
-    %     while j < height(dataGNSS) && dataGNSS(j, 1) < dataIMU(i, 1)
-    %         if dataGNSS(j + 1, 1) > dataIMU(i, 1)
-    %             interp_cnt = interp_cnt + 1;
-    %             % interpolate GNSS
-    %             k = (dataIMU(i, 1) - dataGNSS(j, 1)) / (dataGNSS(j + 1, 1) - dataGNSS(j, 1));
-    %             dataGNSSInterpolated(interp_cnt, 1) = dataIMU(i, 1);
-    %             dataGNSSInterpolated(interp_cnt, 2:end) = dataGNSS(j, 2:end) + k * (dataGNSS(j + 1, 2:end) - dataGNSS(j, 2:end));
-    %             dataIMUInterpolated(interp_cnt, :) = dataIMU(i, :);
-    %             break;
-    %         else
-    %             j = j + 1;
-    %         end
-    %     end
-    % end
-
-
-
     while j <= height(dataGNSS)
         if dataGNSS(j, 1) > dataIMU(i, 1)
             break;
